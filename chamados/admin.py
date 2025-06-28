eb3f8d81c18369742bb65d8899f9bb8b0ee70d0b
NicolasFeula <nicolas.feula@gmail.com>
2025-06-27 23:52:41 -0300

    feat: registra modelos no admin e prepara dados para teste

--- chamados/admin.py
+++ chamados/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Usuario, Categoria, Chamado, Interacao, Anexo
 
-# Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'perfil', 'is_staff', 'is_active', 'date_joined')
+    list_filter = ('perfil', 'is_staff', 'is_active')
+    search_fields = ('username', 'email')
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'descricao')
+    search_fields = ('nome',)
+
+@admin.register(Chamado)
+class ChamadoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titulo', 'status', 'categoria', 'solicitante', 'responsavel', 'data_abertura', 'data_encerramento')
+    list_filter = ('status', 'categoria', 'data_abertura')
+    search_fields = ('titulo', 'descricao')
+
+@admin.register(Interacao)
+class InteracaoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'chamado', 'autor', 'tipo', 'data_interacao')
+    list_filter = ('tipo', 'data_interacao')
+    search_fields = ('mensagem',)
+
+@admin.register(Anexo)
+class AnexoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome_arquivo', 'chamado', 'interacao', 'data_upload')
+    list_filter = ('data_upload',)
+
--- ja_abriu_chamado/settings.py
+++ ja_abriu_chamado/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chamados', # <- adicionado em 27/06
 ]
+
+AUTH_USER_MODEL = 'chamados.Usuario' # <- adicionado em 27/06
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
